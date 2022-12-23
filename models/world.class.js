ec08ad6cd7af9216357e0c1524e8cd7aa57b15ce
Mario Mohar <mariomohar@MacBook-Pro-von-Mario.local>
2022-12-23 10:51:45 +0100

    Add background - not finish

--- models/world.class.js
+++ models/world.class.js
@@ -1,23 +1,24 @@
-class World {
-    character = new Character();
-    enemy = [
+class World { // The World class
+    character = new Character(); // Create a new instance of the Character class
+    enemy = [ // Create an array of Chickens
         new Chickens (),
         new Chickens (),
         new Chickens (),
         new Chickens (),
     ];
-    clouds = [
+    clouds = [ // Create an array of clouds
         new Clouds(),
 
     ];
-    boss = new Boss();
-    canvas;
-    ctx;
+    boss = new Boss(); // Create a new instance of the Boss class
+    background = new Background(); // Create a new instance of the Background class
+    canvas; // The canvas element
+    ctx; // The context of the canvas
 
-    constructor(canvas) {
-        this.ctx = canvas.getContext('2d');
-        this.canvas = canvas;
-        this.draw();
+    constructor(canvas) { // The constructor is called when you create a new instance of the class
+        this.ctx = canvas.getContext('2d'); // Get the context of the canvas needs to drwaw on it i 2d
+        this.canvas = canvas; // Save the canvas in the class
+        this.draw(); // Start the draw loop
     }
 
 
@@ -29,15 +30,17 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear the canvas before drawing
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height); // Draw the world
         this.enemy.forEach((enemy) => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height); // Draw the Chickens
         });
 
         this.clouds.forEach((cloud) => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height); // Draw the Clouds
         });
 
-        this.ctx.drawImage(this.boss.img, this.boss.x, this.boss.y, this.boss.width, this.boss.height); // Draw the world
+        this.ctx.drawImage(this.boss.img, this.boss.x, this.boss.y, this.boss.width, this.boss.height); // Draw the Boss Chicke
 
-        requestAnimationFrame(() => this.draw());
+        this.ctx.drawImage(this.background.img, this.background.x, this.background.y, this.background.width, this.background.height); // Draw the Background
+
+        requestAnimationFrame(() => this.draw()); // Call the draw function again
     }
 } 
